c487a1df92646d5e08bb27f95453482d2af10202
Choongseop Kim <49191949+kchs94@users.noreply.github.com>
2021-05-17 18:13:53 +0900

    User 테이블과 Video 테이블 연동

--- loginapp/models.py
+++ loginapp/models.py
@@ -5,18 +5,20 @@ from django.db import models
 
 # 사용자 모델
 class User(models.Model):
-    name = models.CharField(max_length=6)           # 이름
-    email = models.CharField(max_length=200)        # 이메일
-    password = models.CharField(max_length=200)     # 암호화 하면 문자가 길어지기에
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)                            # 이름
+    email = models.CharField(max_length=200)                         # 이메일
+    password = models.CharField(max_length=200)                      # 암호화 하면 문자가 길어지기에
     create_date = models.DateTimeField(auto_now_add=True)            # 생성일
 
 
-# # 게시판 모델
-# class Board(models.Model):
-#     id = models.ForeignKey(User, on_delete=models.CASCADE)# 각 유저 마다 외래키
-#     object = models.CharField(max_length=200)             # 객체 이름
-#     video = models.CharField(max_length=200)              # 비디오
-#     thumbNail = models.ImageField()                       # 썸네일
-#     dateTime = models.DateTimeField(auto_now_add=True)    # 생성일
+# 게시판 모델
+class Video(models.Model):
+    user_id = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE, db_column="user_id")  # 외래키
+    id = models.AutoField(primary_key=True)               # 비디오 id
+    object = models.CharField(max_length=200)             # 객체 이름
+    video = models.CharField(max_length=200)              # 비디오
+    thumbNail = models.CharField(max_length=200)          # 썸네일
+    dateTime = models.DateTimeField(null=True)            # 생성일
 
 
--- loginapp/views.py
+++ loginapp/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Video
+from .serializers import UserSerializer,  VideoSerializer
 from rest_framework.parsers import JSONParser
 
 
-# 1.user_list - 계정 전체 조회(GET), 회원가입(POST)
+# 1-1.user_list - 계정 전체 조회(GET), 회원가입(POST)
 @csrf_exempt
 def user_list(request):
     if request.method == 'GET':
-        query_set = User.objects.all()  # 모델에 있는 데이터를 query_set으로 다 담기
+        query_set = User.objects.order_by('create_date')  # 모델에 있는 데이터를 query_set으로 다 담기
         serializer = UserSerializer(query_set, many=True)  # User 데이터를 json으로 변환
         return JsonResponse(serializer.data, safe=False)
 
@@ -22,7 +23,7 @@ def user_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 
-# 2.user - pk로 특정 계정 조회(GET), 수정(PUT), 삭제(DELETE)
+# 1-2.user - pk로 특정 계정 조회(GET), 수정(PUT), 삭제(DELETE)
 def user(request, pk):
 
     obj = User.objects.get(pk=pk)  # obj에 한 사용자 넣기
@@ -44,7 +45,7 @@ def user(request, pk):
         return HttpResponse(status=204)
 
 
-# 3.login - 로그인(POST)
+# 1-3.login - 로그인(POST)
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
@@ -57,4 +58,41 @@ def login(request):
         else:
             return HttpResponse(status=400)
 
-# 4.
+# 2-1.video_list
+@csrf_exempt
+def video_list(request):
+    if request.method == 'GET':
+        query_set = Video.objects.all()  # 모델에 있는 데이터를 query_set으로 다 담기
+        serializer = VideoSerializer(query_set, many=True)  # User 데이터를 json으로 변환
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)  # request에 있는 새로운 데이터를 data에 넣기
+        serializer = VideoSerializer(data=data)  # UserSerializer로 Json으로 변환
+        if serializer.is_valid():  # 형식이 맞으면
+            serializer.save()  # 저장
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+# 2-2.user - pk로 특정 계정 조회(GET), 수정(PUT), 삭제(DELETE)
+def video(request, pk):
+
+    obj = User.objects.get(pk=pk)  # obj에 한 사용자 넣기
+
+    if request.method == 'GET':
+        serializer = UserSerializer(obj)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(obj, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        obj.delete()
+        return HttpResponse(status=204)
+
